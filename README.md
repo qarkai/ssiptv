d1d1f2c9eb4384595e2290a2e88fd263f2ec72ec
Arkadiy Illarionov <qarkai@gmail.com>
2021-01-16 20:14:33 +0300

    Add support for LiveProxy

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Nginx script for creating m3u playlist from video directory.
 
+Supported videos by extension: `avi`, `mkv`, `mp4`, `ts`.
+
+Also supports `m3u` and source [LiveProxy](https://liveproxy.github.io/url.html#liveproxy-command) playlists.
+
 ## Usage
 
 Script requires Nginx variable `$video_dir`. Sample `nginx.conf`:
@@ -28,6 +32,11 @@ http {
         location ~ \.m3u$ {
             js_content ssiptv.m3u;
         }
+
+        # playlists with commands for LiveProxy
+        location ~ \.liveproxy$ {
+            js_content ssiptv.liveproxy;
+        }
     }
 }
 ```
@@ -35,3 +44,19 @@ http {
 In SS IPTV add external playlist with url `http://ip:port/.m3u`.
 
 Where `ip` is Nginx's IP, port is from `listen <port>;` directive.
+
+### LiveProxy
+
+Script converts commands from `playlist.liveproxy` files to LiveProxy's base64 urls. Sample file (from LiveProxy docs):
+
+```m3u
+#EXTM3U
+#EXTINF:-1,Arte FR
+streamlink https://www.arte.tv/fr/direct/ 720p,720p_alt,best
+#EXTINF:-1,France24
+streamlink https://www.youtube.com/user/france24 best
+#EXTINF:-1 tvg-id="EuroNews" tvg-name="EuroNews",Euronews
+streamlink https://www.euronews.com/live best
+#EXTINF:-1,France24
+youtube-dl https://www.youtube.com/user/france24/live
+```
--- ssiptv.js
+++ ssiptv.js
@@ -32,6 +32,11 @@ function isVideo(name) {
         || name.endsWith('.ts');
 }
 
+function isPlaylist(name) {
+    return name.endsWith('.liveproxy')
+        || name.endsWith('.m3u');
+}
+
 function filesToM3U(dirContent, host, dir) {
     var extension = /\.[^/.]+$/;
     var s = '#EXTM3U\n';
@@ -50,12 +55,12 @@ function filesToM3U(dirContent, host, dir) {
             type = 'playlist';
             url += '.m3u';
         } else if (item.isFile()) {
-            if (isVideo(item.name)) {
-                logo = 'video-file.png';
-                type = 'video';
-            } else if (item.name.endsWith('.m3u')) {
+            if (isPlaylist(item.name)) {
                 logo = 'video-dir.png';
                 type = 'playlist';
+            } else if (isVideo(item.name)) {
+                logo = 'video-file.png';
+                type = 'video';
             }
         }
 
@@ -78,7 +83,7 @@ function m3u(r) {
         // check if m3u file exists and return it's contents
         var m3uFile = `${localDir}.m3u`;
         fs.accessSync(m3uFile, fs.constants.R_OK);
-        r.return(200, fs.readFileSync(m3uFile), 'utf8');
+        r.return(200, fs.readFileSync(m3uFile));
         return;
     } catch (e) {}
 
@@ -89,4 +94,35 @@ function m3u(r) {
     r.return(200, s);
 }
 
-export default {m3u};
+function liveproxyToM3U(content, host) {
+    var s = '';
+    var lines = content.split(/\r?\n/);
+
+    lines.forEach(line => {
+        if (line === '' || line.startsWith('#')) {
+            s += `${line}\n`;
+        } else {
+            var base64 = line.toString('base64');
+            s += `http://${host}:53422/base64/${base64}/\n`;
+        }
+    }, s);
+
+    return s;
+}
+
+function liveproxy(r) {
+    var path = decodeURI(r.uri.slice(1)); // cut /
+    var localPath = `${r.variables.video_dir}/${path}`;
+
+    try {
+        // check if file exists and convert content to liveproxy links
+        fs.accessSync(localPath, fs.constants.R_OK);
+
+        var content = fs.readFileSync(localPath, 'utf8');
+        var host = r.headersIn.Host.split(':')[0]; // get host part of host:port string
+        var s = liveproxyToM3U(content, host);
+        r.return(200, s);
+    } catch (e) {}
+}
+
+export default {m3u, liveproxy};
